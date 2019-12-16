649a1b994af32c7a964c381e9d4885e1dce32904
Kevin Wang <bird831205@hotmail.com>
2019-12-16 11:02:31 -0500

    writing tests for cavity

---
+++ src/main/java/com/company/Cavity.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Cavity {
+
+
+    static String[] cavityMap(String[] grid) {
+
+        System.out.println(grid);
+        return null;
+    }
+}
---
+++ src/test/java/com/company/CavityTest.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CavityTest {
+
+    public void test(String[] input, String[] expected){
+        String[] actual = Cavity.cavityMap(input);
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void test1() {
+        String[] input = new String[]{"989", "191", "111"};
+        String[] expected = new String[]{"989","1X1","111"};
+        test(input, expected);
+    }
+
+    @Test
+    public void test2() {
+        String[] input = new String[]{"1112", "1912", "1892","1234"};
+        String[] expected = new String[]{"11112","1X12","18X2","1234"};
+        test(input, expected);
+    }
+
+
+}
