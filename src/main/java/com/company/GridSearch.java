8cd86762826c279b75d33f8c717cb729ec899229
Kevin Wang <bird831205@hotmail.com>
2019-12-17 12:52:04 -0500

    trying to find patterns that match the first row of the string

--- src/main/java/com/company/GridSearch.java
+++ src/main/java/com/company/GridSearch.java
@@ -4,15 +4,32 @@ public class GridSearch {
 
     static String gridSearch(String[] G, String[] P) {
 
-        if(containsPattern(G,P)){
+        if (containsPattern(G, P)) {
             return "YES";
-        } else{
+        } else {
             return "NO";
         }
     }
 
     private static boolean containsPattern(String[] g, String[] p) {
-        int width = p[0].length();
+        int patternWidth = p[0].length();
+        int patternHeight = p.length;
+        int gridWidth = g[0].length();
+        int gridHeight = g.length;
+
+        for (int j = 0; j < gridHeight - patternHeight; j++) {
+            for (int i = 0; i < gridWidth - patternWidth; i++) {
+                String row = g[j];
+                if (isEqualFirstRow(row.substring(i,patternWidth+i),p[j])){
+
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isEqualFirstRow(String row1, String row2) {
+        return row1.equals(row2);
     }
 
 
