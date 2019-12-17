7e6a2986541b2629a0a734fe233af18722c57ef0
Kevin Wang <bird831205@hotmail.com>
2019-12-17 09:12:32 -0500

    setting up tests

---
+++ src/main/java/com/company/ManasaAndStones.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ManasaAndStones {
+
+
+    static int[] stones(int n, int a, int b) {
+
+        return null;
+    }
+
+}
---
+++ src/test/java/com/company/StonesTest.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StonesTest {
+
+
+    public void test(int[] expected, int input1, int input2, int input3){
+        int[] input = ManasaAndStones.stones(input1,input2,input3);
+        Assert.assertEquals(expected,input);
+    }
+
+    @Test
+    public void test1(){
+        int[] expected = new int[]{30,120,210,300};
+        test(expected,2,3,4);
+    }
+
+
+
+
+}
