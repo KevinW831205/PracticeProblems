c0a1c1536696727c919bda9c0923f0f59124f5bb
Kevin Wang <bird831205@hotmail.com>
2019-12-17 13:00:48 -0500

    fixing to check lessthan equal

--- src/main/java/com/company/GridSearch.java
+++ src/main/java/com/company/GridSearch.java
@@ -17,8 +17,8 @@ public class GridSearch {
         int gridWidth = g[0].length();
         int gridHeight = g.length;
 
-        for (int j = 0; j < gridHeight - patternHeight; j++) {
-            for (int i = 0; i < gridWidth - patternWidth; i++) {
+        for (int j = 0; j <= gridHeight - patternHeight; j++) {
+            for (int i = 0; i <= gridWidth - patternWidth; i++) {
                 String row = g[j];
                 if (isEqualRow(row.substring(i,patternWidth+i),p[j])){
                     for(int k=0; k<patternHeight; k++){
