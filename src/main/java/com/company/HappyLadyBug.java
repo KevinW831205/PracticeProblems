165ebc67f221468a1991150a82a135bbdefeede0
Kevin Wang <bird831205@hotmail.com>
2019-12-19 16:09:49 -0500

    sorting if contains empty space

--- src/main/java/com/company/HappyLadyBug.java
+++ src/main/java/com/company/HappyLadyBug.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class HappyLadyBug {
 
     static String happyLadybugs(String b) {
+
+        String[] bArr = b.split("");
+        if (b.contains("_")) {
+            List<String> bList = new ArrayList<>(Arrays.asList(bArr));
+            bList.sort(Comparator.naturalOrder());
+            bArr = bList.toArray(new String[0]);
+        }
+
+        for (int i = 0; i < bArr.length-1; i++) {
+
+        }
 
         return null;
     }
