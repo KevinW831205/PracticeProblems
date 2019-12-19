e100811d9a538a2fb0113a8a4d0fd2a875dd5cdb
Kevin Wang <bird831205@hotmail.com>
2019-12-19 16:00:19 -0500

    finish test cases

--- src/test/java/com/company/HappyLadyBugTest.java
+++ src/test/java/com/company/HappyLadyBugTest.java
@@ -1,7 +1,60 @@
 package com.company;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class HappyLadyBugTest {
 
+    public void Test(String expected, String input){
+        //given
 
+        //when
+        String actual = HappyLadyBug.happyLadybugs(input);
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void test1(){
+        Test("RBY_YBR", "YES");
+    }
+
+    @Test
+    public void test2(){
+        Test("X_Y__X", "NO");
+    }
+
+    @Test
+    public void test3(){
+        Test("__", "YES");
+    }
+
+    @Test
+    public void test4(){
+        Test("B_RRBR", "YES");
+    }
+
+    @Test
+    public void test5(){
+        Test("AABBC", "NO");
+    }
+    @Test
+    public void test6(){
+        Test("AABBC_C", "YES");
+    }
+    @Test
+    public void test7(){
+        Test("_", "YES");
+    }
+    @Test
+    public void test8(){
+        Test("DD__FQ_QQF", "YES");
+    }
+
+    @Test
+    public void test9(){
+        Test("AABCBC", "NO");
+    }
 
 }
+
