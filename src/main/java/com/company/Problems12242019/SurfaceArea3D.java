9ee9bacd307ea77981bb2893594788e6b23f6b11
KevinW <bird831205@hotmail.com>
2019-12-24 09:08:53 -0500

    writing test cases and setting up 3DSurfaceAres

--- src/main/java/com/company/Problems12242019/SurfaceArea3D.java
+++ src/main/java/com/company/Problems12242019/SurfaceArea3D.java
@@ -2,5 +2,10 @@ package com.company.Problems12242019;
 
 public class SurfaceArea3D {
 
+    public static int surfaceArea(int[][] A) {
+
+        return -1;
+    }
+
 
 }
--- src/test/java/com/company/SurfaceArea3DTest.java
+++ src/test/java/com/company/SurfaceArea3DTest.java
@@ -1,4 +1,20 @@
 package com.company;
 
+import com.company.Problems12242019.SurfaceArea3D;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SurfaceArea3DTest {
+
+    public void test(int expected, int[][] input){
+        int actual =  SurfaceArea3D.surfaceArea(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test1(){
+        int[][] input = new int[][]{new int[]{1,3,4}, new int[]{2,2,3}, new int[]{1,2,4}};
+        test(60, input);
+    }
+
 }
