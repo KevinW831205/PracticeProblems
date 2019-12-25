777bc15e9074e032eae3264e1b040741673b6cb0
KevinW <bird831205@hotmail.com>
2019-12-25 05:17:29 -0500

    topbottomarea now considers length

--- src/main/java/com/company/Problems12242019/SurfaceArea3D.java
+++ src/main/java/com/company/Problems12242019/SurfaceArea3D.java
@@ -4,9 +4,16 @@ public class SurfaceArea3D {
 
     public static int surfaceArea(int[][] A) {
 
-        int areaTopAndBottom =  A.length * A.length *2;
+        int areaTopAndBottom =  A.length * A[0].length *2;
 
 
+
+        /*
+        60
+        1,3,4
+        2,2,3
+        1,2,4
+         */
 
         return -1;
     }
