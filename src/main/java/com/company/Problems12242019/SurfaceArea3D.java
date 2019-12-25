06fa64abcb5fcb490b25d405f4c5cd45d5919920
KevinW <bird831205@hotmail.com>
2019-12-25 05:41:13 -0500

    debugged not having Math.absolute

--- src/main/java/com/company/Problems12242019/SurfaceArea3D.java
+++ src/main/java/com/company/Problems12242019/SurfaceArea3D.java
@@ -11,15 +11,16 @@ public class SurfaceArea3D {
         for (int i = 0; i < A.length; i++) {
             areaLeftRight += A[i][0] + A[i][A[i].length - 1];
             for (int j = 1; j < A[i].length; j++) {
-                areaLeftRight += A[i][j] - A[i][j-1];
+                areaLeftRight += Math.abs(A[i][j] - A[i][j - 1]);
             }
         }
 
-        for (int i = 0; i < A[i].length; i++) {
-            areaForwardBackward += A[0][i] +A[A[0].length-1][i];
+        for (int i = 0; i < A[0].length; i++) {
+            areaForwardBackward += A[0][i] + A[A[0].length - 1][i];
             for (int j = 1; j < A.length; j++) {
-                areaForwardBackward += A[j][i] - A[j-1][i];
+                areaForwardBackward += Math.abs(A[j][i] - A[j - 1][i]);
             }
+            System.out.println("i " + areaForwardBackward);
         }
 
         /*
@@ -28,6 +29,10 @@ public class SurfaceArea3D {
         2,2,3       6
         1,2,4       8     4 6 10    9 9
          */
+
+        System.out.println("top bottom " + areaTopAndBottom);
+        System.out.println("Left Right " + areaLeftRight);
+        System.out.println("Forward Backward" + areaForwardBackward);
 
         return areaTopAndBottom + areaLeftRight + areaForwardBackward;
 
