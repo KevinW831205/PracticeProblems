7b6830776ebb01de7f57e22a29885d87f9994bb3
KevinW <bird831205@hotmail.com>
2019-12-28 15:58:30 +0800

    looking at some other solution

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,22 +1,23 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        List<String> l1 = new ArrayList<String>();
+//        List<String> l1 = new ArrayList<String>();
+//
+//      639 000 000 000
+//
+//        l1.add("a");
+//        l1.add("a");
+//        l1.add("b");
+//        l1.remove("a");
+//
+//        System.out.println(l1);
 
-
-
-        l1.add("a");
-        l1.add("a");
-        l1.add("b");
-        l1.remove("a");
-
-        System.out.println(l1);
+        int x = Integer.MAX_VALUE;
+        System.out.println("x " +x);
+        System.out.println("x+1 "+(x+1));
 
 
     }
--- src/main/java/com/company/Problems12242019/SurfaceArea3D.java
+++ src/main/java/com/company/Problems12242019/SurfaceArea3D.java
@@ -4,24 +4,54 @@ public class SurfaceArea3D {
 
     public static int surfaceArea(int[][] A) {
 
-        int areaTopAndBottom = A.length * A[0].length * 2;
-        int areaForwardBackward = 0;
-        int areaLeftRight = 0;
+//        int areaTopAndBottom = A.length * A[0].length * 2;
+//        int areaForwardBackward = 0;
+//        int areaLeftRight = 0;
+//
+//        for (int i = 0; i < A.length; i++) {
+//            areaLeftRight += A[i][0] + A[i][A[i].length - 1];
+//            for (int j = 1; j < A[i].length; j++) {
+//                areaLeftRight += Math.abs(A[i][j] - A[i][j - 1]);
+//            }
+//        }
+//
+//        for (int i = 0; i < A[0].length; i++) {
+//            areaForwardBackward += A[0][i] + A[A[0].length - 1][i];
+//            for (int j = 1; j < A.length; j++) {
+//                areaForwardBackward += Math.abs(A[j][i] - A[j - 1][i]);
+//            }
+//            System.out.println("i " + areaForwardBackward);
+//        }
+//
+//        return areaTopAndBottom + areaLeftRight + areaForwardBackward;
+
+
+        // Someone else's solution
+        int area = 0;
 
         for (int i = 0; i < A.length; i++) {
-            areaLeftRight += A[i][0] + A[i][A[i].length - 1];
-            for (int j = 1; j < A[i].length; j++) {
-                areaLeftRight += Math.abs(A[i][j] - A[i][j - 1]);
+            int areaOfOneRow = A[i][0];
+            for (int j = 0; j < A[i].length - 1; j++) {
+                if (A[i][j + 1] > A[i][j]) {
+                    areaOfOneRow += A[i][j + 1] - A[i][j];
+                }
             }
+            area += areaOfOneRow * 2;
         }
 
         for (int i = 0; i < A[0].length; i++) {
-            areaForwardBackward += A[0][i] + A[A[0].length - 1][i];
-            for (int j = 1; j < A.length; j++) {
-                areaForwardBackward += Math.abs(A[j][i] - A[j - 1][i]);
+            int areaOfOneColumn = A[0][i];
+            for (int j = 0; j < A.length - 1; j++) {
+                if (A[j + 1][i] > A[j][i]) {
+                    areaOfOneColumn += A[j + 1][i] - A[j][i];
+                }
             }
-            System.out.println("i " + areaForwardBackward);
+            area += areaOfOneColumn * 2;
         }
+
+        area += A.length * A[0].length * 2;
+
+        return area;
 
         /*
         60
@@ -30,11 +60,10 @@ public class SurfaceArea3D {
         1,2,4       8     4 6 10    9 9
          */
 
-        System.out.println("top bottom " + areaTopAndBottom);
-        System.out.println("Left Right " + areaLeftRight);
-        System.out.println("Forward Backward" + areaForwardBackward);
+//        System.out.println("top bottom " + areaTopAndBottom);
+//        System.out.println("Left Right " + areaLeftRight);
+//        System.out.println("Forward Backward" + areaForwardBackward);
 
-        return areaTopAndBottom + areaLeftRight + areaForwardBackward;
 
     }
 
