339fa1753ba0b4b8cbbcc0be32cb9f7f26821d92
Kevin Wang <bird831205@hotmail.com>
2020-01-08 12:30:57 -0500

    initial implemetation

--- src/main/java/com/company/AbsolutePermutation.java
+++ src/main/java/com/company/AbsolutePermutation.java
@@ -2,8 +2,18 @@ package com.company;
 
 public class AbsolutePermutation {
     static int[] absolutePermutation(int n, int k) {
+        int[] result = new int[n];
 
-        return new int[]{};
+        for(int i =1; i<=n; i++){
+            if(i+k>=1 && i+k <= n){
+                result[i-1] = i+k;
+            } else if(i-k>=1 && i-k <=n){
+                result[i-1] = i-k;
+            } else {
+                return new int[]{-1};
+            }
+        }
+        return result;
     }
 
 }
