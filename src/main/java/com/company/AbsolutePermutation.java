ea1e931682fec80bf1c62cb78d434f3b5f2cfc7c
Kevin Wang <bird831205@hotmail.com>
2020-01-08 12:20:58 -0500

    testing for absoluteperumutation

---
+++ src/main/java/com/company/AbsolutePermutation.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class AbsolutePermutation {
+    static int[] absolutePermutation(int n, int k) {
+
+        return new int[]{};
+    }
+
+}
---
+++ src/test/java/com/company/AbsolutePermutationTest.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AbsolutePermutationTest {
+
+    public void absolutePermutationTest(int[] expected, int n, int k){
+        int[] actual =  AbsolutePermutation.absolutePermutation(n,k);
+        Assert.assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void Test1(){
+        int[] expected = new int[]{2,1};
+        absolutePermutationTest(expected,2,1);
+    }
+
+    @Test
+    public void Test2(){
+        int[] expected = new int[]{1,2,3};
+        absolutePermutationTest(expected,3,0);
+
+    }
+
+    @Test
+    public void Test3(){
+        int[] expected = new int[]{-1};
+        absolutePermutationTest(expected,3,2);
+    }
+
+    @Test
+    public void Test4(){
+        int[] expected = new int[]{6,7,8,9,10,1,2,3,4,5};
+        absolutePermutationTest(expected,10,5);
+    }
+
+}
