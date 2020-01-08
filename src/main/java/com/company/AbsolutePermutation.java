c21be4f5e6d5189c297d0589f1b45e1a12f5f856
Kevin Wang <bird831205@hotmail.com>
2020-01-08 14:16:44 -0500

    new approach

--- src/main/java/com/company/AbsolutePermutation.java
+++ src/main/java/com/company/AbsolutePermutation.java
@@ -6,38 +6,61 @@ import java.util.Set;
 
 public class AbsolutePermutation {
     static int[] absolutePermutation(int n, int k) {
-        Set<Integer> added = new HashSet<>();
         int[] result = new int[n];
+        Set<Integer> added = new HashSet<>();
 
-        if (k != 0 && n % k == 0) {
+        if (k == 0) {
             for (int i = 1; i <= n; i++) {
-                System.out.println(i);
-                if (i - k >= 1 && i - k <= n && !added.contains(i - k)) {
-                    result[i - 1] = i - k;
-                    added.add(i - k);
-                } else if (i + k >= 1 && i + k <= n && !added.contains(i + k)) {
+                result[i - 1] = i;
+            }
+        } else if (k == n / 2) {
+            for (int i = 1; i <= n; i++) {
+                if (i + k >= 1 && i + k <= n) {
                     result[i - 1] = i + k;
-                    added.add(i + k);
-                } else {
-                    return new int[]{-1};
+                } else if (i - k >= 1 && i - k <= n) {
+                    result[i - 1] = i - k;
                 }
             }
-        } else if(k == n/2 || k==0) {
-            for (int i = 1; i <= n; i++) {
-                System.out.println(i);
-                if (i + k >= 1 && i + k <= n && !added.contains(i + k)) {
-                    result[i - 1] = i + k;
-                    added.add(i + k);
-                } else if (i - k >= 1 && i - k <= n && !added.contains(i - k)) {
-                    result[i - 1] = i - k;
-                    added.add(i - k);
-                } else {
-                    return new int[]{-1};
+        } else if (n % (k*2) == 0) {
+            for (int i = 1; i <= n / k; i++) {
+                for (int j = 1; j <= k; j++) {
+                    int index = k*(i-1)+j-1;
+                    if (j % 2 == 1) {
+                        result[index] = k * i + j;
+                    } else {
+                        result[index] = k * i - j;
+                    }
                 }
             }
         } else {
             return new int[]{-1};
         }
+
+//        if (k != 0 && n % k == 0) {
+//            for (int i = 1; i <= n; i++) {
+//                System.out.println(i);
+//                if (i - k >= 1 && i - k <= n && !added.contains(i - k)) {
+//                    result[i - 1] = i - k;
+//                    added.add(i - k);
+//                } else if (i + k >= 1 && i + k <= n && !added.contains(i - k)) {
+//                    result[i - 1] = i + k;
+//                    added.add(i + k);
+//                }
+//            }
+//        } else if (k == n / 2 || k == 0) {
+//            for (int i = 1; i <= n; i++) {
+//                System.out.println(i);
+//                if (i + k >= 1 && i + k <= n) {
+//                    result[i - 1] = i + k;
+//                } else if (i - k >= 1 && i - k <= n) {
+//                    result[i - 1] = i - k;
+//                } else {
+//                    return new int[]{-1};
+//                }
+//            }
+//        } else {
+//            return new int[]{-1};
+//        }
 
         return result;
     }
