9d93c0a93c22607c847b1603e2cf5d7085e26a21
KevinW <bird831205@hotmail.com>
2020-01-10 17:08:07 -0500

    last few cases

--- src/main/java/com/company/BomberMan.java
+++ src/main/java/com/company/BomberMan.java
@@ -34,16 +34,22 @@ public class BomberMan {
                             result[i - 1] = result[i - 1].substring(0, result.length - 1) + ".";
                         } else if (i == 0) {
                             result[i] = result[i].substring(0, j - 1) + "..." + result[i].substring(j + 2);
-                            result[i+1] = result[i+1].substring(0,j) +"."+result[i+1].substring(j+1);
+                            result[i + 1] = result[i + 1].substring(0, j) + "." + result[i + 1].substring(j + 1);
                         } else if (j == 0) {
-
+                            result[i] = ".." + result[i].substring(2);
+                            result[i + 1] = "." + result[i + 1].substring(1);
+                            result[i - 1] = "." + result[i - 1].substring(1);
                         } else if (i == result.length - 1) {
                             result[i] = result[i].substring(0, j - 1) + "..." + result[i].substring(j + 2);
-                            result[i-1] = result[i-1].substring(0,j) +"."+result[i-1].substring(j+1);
+                            result[i - 1] = result[i - 1].substring(0, j) + "." + result[i - 1].substring(j + 1);
                         } else if (j == result.length - 1) {
-
+                            result[i] = result[i].substring(0, j - 1) + "..";
+                            result[i + 1] = result[i + 1].substring(0, j) + "." ;
+                            result[i - 1] = result[i - 1].substring(0, j) + "." ;
                         } else {
-
+                            result[i] = result[i].substring(0, j - 1) + "..." + result[i].substring(j + 2);
+                            result[i + 1] = result[i + 1].substring(0, j) + "." + result[i + 1].substring(j + 1);
+                            result[i - 1] = result[i - 1].substring(0, j) + "." + result[i - 1].substring(j + 1);
                         }
                     }
                 }
