30716fe127db787a6ed3dcc06e5785ec3a0abf5e
KevinW <bird831205@hotmail.com>
2020-01-10 17:22:04 -0500

    more debug

--- src/main/java/com/company/BomberMan.java
+++ src/main/java/com/company/BomberMan.java
@@ -20,20 +20,18 @@ public class BomberMan {
             System.out.println("length" + result.length);
             for (int i = 0; i < grid2SecAgo.length; i++) {
                 for (int j = 0; j < grid2SecAgo[i].length(); j++) {
-                    System.out.println("i" + i);
-                    System.out.println("j" + j);
                     if (grid2SecAgo[i].charAt(j) == 'O') {
                         if (j == 0 && i == 0) {
                             result[i] = ".." + result[i].substring(2);
                             result[i + 1] = "." + result[i + 1].substring(1);
                         } else if (i == 0 && j == grid2SecAgo[i].length() - 1) {
-                            result[i] = result[i].substring(0, result.length - 2) + "..";
+                            result[i] = result[i].substring(0, result.length - 1) + "..";
                             result[i + 1] = result[i + 1].substring(0, result.length - 1) + ".";
                         } else if (i == result.length - 1 && j == 0) {
                             result[i] = ".." + result[i].substring(2);
                             result[i - 1] = "." + result[i - 1].substring(1);
                         } else if (i == result.length - 1 && j == grid2SecAgo[i].length() - 1) {
-                            result[i] = result[i].substring(0, result.length - 2) + "..";
+                            result[i] = result[i].substring(0, result.length - 1) + "..";
                             result[i - 1] = result[i - 1].substring(0, result.length - 1) + ".";
                         } else if (i == 0) {
                             result[i] = result[i].substring(0, j - 1) + "..." + result[i].substring(j + 2);
@@ -57,6 +55,7 @@ public class BomberMan {
                     }
                 }
             }
+            System.out.println(result);
             return result;
         }
     }
