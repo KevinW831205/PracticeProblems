06b30eb3b7fefd953cb85cb361f2549717f0caf3
KevinW <bird831205@hotmail.com>
2020-01-10 17:17:17 -0500

    some debugging

--- src/main/java/com/company/BomberMan.java
+++ src/main/java/com/company/BomberMan.java
@@ -17,19 +17,22 @@ public class BomberMan {
             return result;
         } else {
             String[] grid2SecAgo = bomberMan(n - 2, grid);
+            System.out.println("length" + result.length);
             for (int i = 0; i < grid2SecAgo.length; i++) {
                 for (int j = 0; j < grid2SecAgo[i].length(); j++) {
+                    System.out.println("i" + i);
+                    System.out.println("j" + j);
                     if (grid2SecAgo[i].charAt(j) == 'O') {
                         if (j == 0 && i == 0) {
                             result[i] = ".." + result[i].substring(2);
                             result[i + 1] = "." + result[i + 1].substring(1);
-                        } else if (i == 0 && j == result.length - 1) {
+                        } else if (i == 0 && j == grid2SecAgo[i].length() - 1) {
                             result[i] = result[i].substring(0, result.length - 2) + "..";
                             result[i + 1] = result[i + 1].substring(0, result.length - 1) + ".";
                         } else if (i == result.length - 1 && j == 0) {
                             result[i] = ".." + result[i].substring(2);
-                            result[i + 1] = "." + result[i + 1].substring(1);
-                        } else if (i == result.length - 1 && j == result.length - 1) {
+                            result[i - 1] = "." + result[i - 1].substring(1);
+                        } else if (i == result.length - 1 && j == grid2SecAgo[i].length() - 1) {
                             result[i] = result[i].substring(0, result.length - 2) + "..";
                             result[i - 1] = result[i - 1].substring(0, result.length - 1) + ".";
                         } else if (i == 0) {
@@ -42,10 +45,10 @@ public class BomberMan {
                         } else if (i == result.length - 1) {
                             result[i] = result[i].substring(0, j - 1) + "..." + result[i].substring(j + 2);
                             result[i - 1] = result[i - 1].substring(0, j) + "." + result[i - 1].substring(j + 1);
-                        } else if (j == result.length - 1) {
+                        } else if (j == grid2SecAgo[i].length() - 1) {
                             result[i] = result[i].substring(0, j - 1) + "..";
-                            result[i + 1] = result[i + 1].substring(0, j) + "." ;
-                            result[i - 1] = result[i - 1].substring(0, j) + "." ;
+                            result[i + 1] = result[i + 1].substring(0, j) + ".";
+                            result[i - 1] = result[i - 1].substring(0, j) + ".";
                         } else {
                             result[i] = result[i].substring(0, j - 1) + "..." + result[i].substring(j + 2);
                             result[i + 1] = result[i + 1].substring(0, j) + "." + result[i + 1].substring(j + 1);
@@ -54,7 +57,7 @@ public class BomberMan {
                     }
                 }
             }
-            return grid;
+            return result;
         }
     }
 }
