a5da5db71b85639eeb39b0b7f020c814fc596f10
KevinW <bird831205@hotmail.com>
2020-01-10 09:03:33 -0500

    wrapup

--- src/main/java/com/company/BomberMan.java
+++ src/main/java/com/company/BomberMan.java
@@ -4,20 +4,24 @@ public class BomberMan {
 
     static String[] bomberMan(int n, String[] grid) {
         String[] result = new String[grid.length];
-
+        for (int i = 0; i < grid.length; i++) {
+            String row = "";
+            for (int j = 0; j < grid[i].length(); j++) {
+                row += "O";
+            }
+            result[i] = row;
+        }
         if(n == 1){
             return grid;
         } else if (n % 2 == 0) {
-            for (int i = 0; i < grid.length; i++) {
-                String row = "";
-                for (int j = 0; j < grid[i].length(); j++) {
-                    row += "O";
-                }
-                result[i] = row;
-            }
+            return result;
         } else {
+            String[] grid2SecAgo = bomberMan(n-2,grid);
+            for(int i=0; i<grid2SecAgo.length; i++){
+
+            }
+
         }
         return result;
     }
-
 }
