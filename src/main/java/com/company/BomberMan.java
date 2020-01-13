7a6eeba08d2a132d5f7f1318dbb3ee67d6174e78
Kevin Wang <bird831205@hotmail.com>
2020-01-13 12:29:59 -0500

    checking if cross

--- src/main/java/com/company/BomberMan.java
+++ src/main/java/com/company/BomberMan.java
@@ -2,78 +2,79 @@ package com.company;
 
 public class BomberMan {
 
-//    static String[] bomberMan(int n, String[] grid) {
-//        String[] result = new String[grid.length];
-//        for (int i = 0; i < grid.length; i++) {
-//            String row = "";
-//            for (int j = 0; j < grid[i].length(); j++) {
-//                row += "O";
-//            }
-//            result[i] = row;
-//        }
-//        if (n == 1) {
-//            return grid;
-//        } else if (n % 2 == 0) {
-//            return result;
-//        } else {
-//            String[] grid2SecAgo = bomberMan(n - 2, grid);
-//            for (int i = 0; i < grid2SecAgo.length; i++) {
-////                System.out.println("at i" + i);
-////                System.out.println("bp: " + grid2SecAgo[0]);
-////                System.out.println("before: " + result[0]);
-//                for (int j = 0; j < grid2SecAgo[i].length(); j++) {
-//                    if (grid2SecAgo[i].charAt(j) == 'O') {
-//                        if (j == 0 && i == 0) {
-//                            result[i] = ".." + result[i].substring(2);
-//                            result[i + 1] = "." + result[i + 1].substring(1);
-//                        } else if (i == 0 && j == grid2SecAgo[i].length() - 1) {
-//                            result[i] = result[i].substring(0, result[i].length() - 2) + "..";
-//                            result[i + 1] = result[i + 1].substring(0, result[i + 1].length() - 1) + ".";
-//                        } else if (i == result.length - 1 && j == 0) {
-//                            result[i] = ".." + result[i].substring(2);
-//                            result[i - 1] = "." + result[i - 1].substring(1);
-//                        } else if (i == result.length - 1 && j == grid2SecAgo[i].length() - 1) {
-//                            result[i] = result[i].substring(0, result[i].length() - 2) + "..";
-//                            result[i - 1] = result[i - 1].substring(0, result[i - 1].length() - 1) + ".";
-//                        } else if (i == 0) {
-//                            result[i] = result[i].substring(0, j - 1) + "..." + result[i].substring(j + 2);
-//                            result[i + 1] = result[i + 1].substring(0, j) + "." + result[i + 1].substring(j + 1);
-//                        } else if (j == 0) {
-//                            result[i] = ".." + result[i].substring(2);
-//                            result[i + 1] = "." + result[i + 1].substring(1);
-//                            result[i - 1] = "." + result[i - 1].substring(1);
-//                        } else if (i == result.length - 1) {
-//                            result[i] = result[i].substring(0, j - 1) + "..." + result[i].substring(j + 2);
-//                            result[i - 1] = result[i - 1].substring(0, j) + "." + result[i - 1].substring(j + 1);
-//                        } else if (j == grid2SecAgo[i].length() - 1) {
-//                            result[i] = result[i].substring(0, j - 1) + "..";
-//                            result[i + 1] = result[i + 1].substring(0, j) + ".";
-//                            result[i - 1] = result[i - 1].substring(0, j) + ".";
-//                        } else {
-//                            result[i] = result[i].substring(0, j - 1) + "..." + result[i].substring(j + 2);
-//                            result[i + 1] = result[i + 1].substring(0, j) + "." + result[i + 1].substring(j + 1);
-//                            result[i - 1] = result[i - 1].substring(0, j) + "." + result[i - 1].substring(j + 1);
-//                        }
+    static String[] bomberMan(int n, String[] grid) {
+        String[] result = new String[grid.length];
+        for (int i = 0; i < grid.length; i++) {
+            String row = "";
+            for (int j = 0; j < grid[i].length(); j++) {
+                row += "O";
+            }
+            result[i] = row;
+        }
+        if (n == 1) {
+            return grid;
+        } else if (n % 2 == 0) {
+            return result;
+        } else {
+            String[] grid2SecAgo = bomberMan(n - 2, grid);
+            for (int i = 0; i < grid2SecAgo.length; i++) {
+//                System.out.println("at i" + i);
+//                System.out.println("bp: " + grid2SecAgo[0]);
+//                System.out.println("before: " + result[0]);
+                for (int j = 0; j < grid2SecAgo[i].length(); j++) {
+                    if (grid2SecAgo[i].charAt(j) == 'O') {
+                        if (j == 0 && i == 0) {
+                            result[i] = ".." + result[i].substring(2);
+                            result[i + 1] = "." + result[i + 1].substring(1);
+                        } else if (i == 0 && j == grid2SecAgo[i].length() - 1) {
+                            result[i] = result[i].substring(0, result[i].length() - 2) + "..";
+                            result[i + 1] = result[i + 1].substring(0, result[i + 1].length() - 1) + ".";
+                        } else if (i == result.length - 1 && j == 0) {
+                            result[i] = ".." + result[i].substring(2);
+                            result[i - 1] = "." + result[i - 1].substring(1);
+                        } else if (i == result.length - 1 && j == grid2SecAgo[i].length() - 1) {
+                            result[i] = result[i].substring(0, result[i].length() - 2) + "..";
+                            result[i - 1] = result[i - 1].substring(0, result[i - 1].length() - 1) + ".";
+                        } else if (i == 0) {
+                            result[i] = result[i].substring(0, j - 1) + "..." + result[i].substring(j + 2);
+                            result[i + 1] = result[i + 1].substring(0, j) + "." + result[i + 1].substring(j + 1);
+                        } else if (j == 0) {
+                            result[i] = ".." + result[i].substring(2);
+                            result[i + 1] = "." + result[i + 1].substring(1);
+                            result[i - 1] = "." + result[i - 1].substring(1);
+                        } else if (i == result.length - 1) {
+                            result[i] = result[i].substring(0, j - 1) + "..." + result[i].substring(j + 2);
+                            result[i - 1] = result[i - 1].substring(0, j) + "." + result[i - 1].substring(j + 1);
+                        } else if (j == grid2SecAgo[i].length() - 1) {
+                            result[i] = result[i].substring(0, j - 1) + "..";
+                            result[i + 1] = result[i + 1].substring(0, j) + ".";
+                            result[i - 1] = result[i - 1].substring(0, j) + ".";
+                        } else {
+                            result[i] = result[i].substring(0, j - 1) + "..." + result[i].substring(j + 2);
+                            result[i + 1] = result[i + 1].substring(0, j) + "." + result[i + 1].substring(j + 1);
+                            result[i - 1] = result[i - 1].substring(0, j) + "." + result[i - 1].substring(j + 1);
+                        }
+                    }
+//                    if (i == 0) {
+//                        System.out.println("j" + j + " " + result[0]);
 //                    }
-////                    if (i == 0) {
-////                        System.out.println("j" + j + " " + result[0]);
-////                    }
+                }
+//                System.out.println("after: " + result[0]);
+
+//                for(String s : result){
+//                    System.out.println(s);
 //                }
-////                System.out.println("after: " + result[0]);
-//
-////                for(String s : result){
-////                    System.out.println(s);
-////                }
+            }
+//            System.out.println("sec = " + n);
+//            for (String s : result) {
+//                System.out.println(s);
 //            }
-////            System.out.println("sec = " + n);
-////            for (String s : result) {
-////                System.out.println(s);
-////            }
-//            return result;
-//        }
-//    }
+            return result;
+        }
+    }
 
     // someone elses solution
+    /*
     static String[] bomberMan(int n, String[] grid) {
         String[] ans = new String[grid.length];
         int aa = grid[0].length();
@@ -226,5 +227,7 @@ public class BomberMan {
         }
         return(ans);
     }
+
+     */
 }
 
--- src/main/java/com/company/EmaSuperComputer.java
+++ src/main/java/com/company/EmaSuperComputer.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.sun.scenario.effect.Offset;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,12 +13,21 @@ public class EmaSuperComputer {
         Integer pulse2 = 1;
 
         List<String[]> temp = new ArrayList<>();
-        for(String s : grid){
+        for (String s : grid) {
             String[] row = s.split("");
             temp.add(row);
         }
         String[][] arrGrid = temp.toArray(new String[0][0]);
 
+        //i is row j is col
+        for (int i = 1; i < arrGrid.length; i++) {
+            for (int j = 1; j < arrGrid[i].length; j++) {
+                int offset = 1;
+                if (arrGrid[i + offset][j].equals("G") && arrGrid[i - offset][j].equals("G") && arrGrid[i][j + offset].equals("G") && arrGrid[i][j - offset].equals("G")) {
+
+                }
+            }
+        }
 
         return pulse1 * pulse2;
     }
