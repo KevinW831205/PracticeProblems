0c60f98951c296964127364461339dbfa37e0c09
Kevin Wang <bird831205@hotmail.com>
2020-01-13 11:45:31 -0500

    creating grid as String[][]

--- src/main/java/com/company/EmaSuperComputer.java
+++ src/main/java/com/company/EmaSuperComputer.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class EmaSuperComputer {
 
     static int twoPluses(String[] grid) {
-        return 0;
+        Integer pulse1 = 1;
+        Integer pulse2 = 1;
+
+        List<String[]> temp = new ArrayList<>();
+        for(String s : grid){
+            String[] row = s.split("");
+            temp.add(row);
+        }
+        String[][] arrGrid = temp.toArray(new String[0][0]);
+
+
+        return pulse1 * pulse2;
     }
 }
