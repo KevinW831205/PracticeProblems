c5868786a52f3bb2c2cc79f2b7b3a753be2b117d
Kevin Wang <bird831205@hotmail.com>
2020-01-14 14:10:37 -0500

    some other persons solution

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,28 +1,9 @@
 package com.company;
 
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-
-//        List<String> l1 = new ArrayList<String>();
-//
-//      639 000 000 000
-//
-//        l1.add("a");
-//        l1.add("a");
-//        l1.add("b");
-//        l1.remove("a");
-//
-//        System.out.println(l1);
-
-//        int x = Integer.MAX_VALUE;
-//        System.out.println("x " +x);
-//        System.out.println("x+1 "+(x+1));
-
-        String s = "0123456789";
-        System.out.println(s.substring(1));
-        System.out.println(s.substring(0,5)+"."+s.substring(5+1));
-        System.out.println(s.substring(4,7));
-        System.out.println(s.substring(0,s.length()-1));
     }
 }
