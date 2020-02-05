970f1878e4fd10f8e308e77c7a79d985fc471d8f
KevinW <bird831205@hotmail.com>
2020-02-05 09:01:58 -0500

    wrapup

--- src/main/java/com/company/Calc.java
+++ src/main/java/com/company/Calc.java
@@ -2,8 +2,37 @@ package com.company;
 
 public class Calc {
 
-    public double evaluate(String expr){
+    public double evaluate(String expr) {
+        String[] tokens = expr.split(" ");
+
+        for (int i = 0; i < tokens.length; i++) {
+            Double val;
+            switch (tokens[i]) {
+                case "+":
+                    val = Double.parseDouble(tokens[i - 2]) + Double.parseDouble(tokens[i - 1]);
+                    break;
+                case "-":
+                    val = Double.parseDouble(tokens[i - 2]) - Double.parseDouble(tokens[i - 1]);
+                    break;
+                case "*":
+                    val = Double.parseDouble(tokens[i - 2]) * Double.parseDouble(tokens[i - 1]);
+                    break;
+                case "/":
+                    val = Double.parseDouble(tokens[i - 2]) / Double.parseDouble(tokens[i - 1]);
+                    break;
+                default:
+                    continue;
+            }
+            splice(tokens,i,2,val);
+        }
 
         return 0;
     }
+
+    private void splice(String[] tokens, int index, int numberToRemove, Double val) {
+
+    }
+
+
 }
+
