23c2e40a5d58e5c2690101487497de7b04c0a5ee
KevinW <bird831205@hotmail.com>
2020-02-05 17:01:37 -0500

    somewhat working

--- src/main/java/com/company/Calc.java
+++ src/main/java/com/company/Calc.java
@@ -9,6 +9,9 @@ public class Calc {
 
         for (int i = 0; i < tokens.length; i++) {
             Double val;
+            if (tokens[i].matches("[0-9]+")) {
+                continue;
+            }
             switch (tokens[i]) {
                 case "+":
                     val = Double.parseDouble(tokens[i - 2]) + Double.parseDouble(tokens[i - 1]);
@@ -25,7 +28,14 @@ public class Calc {
                 default:
                     continue;
             }
-            splice(tokens, i, 3, val);
+
+            if (tokens.length == 3) {
+                return val;
+            }
+
+            tokens =  splice(tokens, i-2, 3, val);
+
+            i = i - 3;
         }
 
         return 0;
@@ -33,13 +43,12 @@ public class Calc {
 
     public String[] splice(String[] tokens, int index, int numberToRemove, Double val) {
         String[] result = Arrays.copyOf(tokens, tokens.length - numberToRemove + 1);
-        for (int i = 0; i<index; i++){
+        result[index] = val.toString();
+        for (int i = 0; i < index; i++) {
             result[i] = tokens[i];
-            result[index] = val.toString();
         }
-        System.out.println(result.length);
-        for(int i=index+1; i<result.length; i++){
-            result[i] = tokens[i+numberToRemove-1];
+        for (int i = index + 1; i < result.length; i++) {
+            result[i] = tokens[i + numberToRemove - 1];
         }
         return result;
     }
--- src/test/java/com/company/CalcTest.java
+++ src/test/java/com/company/CalcTest.java
@@ -5,26 +5,26 @@ import org.junit.Test;
 
 public class CalcTest {
 
-//    public void evalTest(double expected, String input){
-//        Calc c = new Calc();
-//
-//        double result = c.evaluate(input);
-//
-//        Assert.assertEquals(expected,result);
-//    }
-//
-//    @Test
-//    public void evalTest1(){
-//        evalTest(14,"5 1 2 + 4 * + 3 -");
-//    }
+    public void evalTest(double expected, String input){
+        Calc c = new Calc();
+
+        double result = c.evaluate(input);
+
+        Assert.assertEquals(expected,result,0.1);
+    }
+
+    @Test
+    public void evalTest1(){
+        evalTest(14,"5 1 2 + 4 * + 3 -");
+    }
 
     @Test
     public void spliceTest(){
         Calc c = new Calc();
-        String[] initial = "5 1 2 + 4 * + 3 -".split(" ");
-        String[] result = c.splice(initial,1,3, 3D);
+        String[] initial = "5 12.0 + 3 -".split(" ");
+        String[] result = c.splice(initial,0,3, 17D);
 
-        Assert.assertArrayEquals("5 3.0 4 * + 3 -".split(" "), result);
+        Assert.assertArrayEquals("17.0 3 -".split(" "), result);
     }
 
 }
