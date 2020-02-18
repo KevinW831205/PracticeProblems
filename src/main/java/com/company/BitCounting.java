d58a561b55ef9e9a9fa97e70aa3f60a20326a493
KevinW <bird831205@hotmail.com>
2020-02-18 08:25:53 -0500

    simple solution

--- src/main/java/com/company/BitCounting.java
+++ src/main/java/com/company/BitCounting.java
@@ -3,7 +3,14 @@ package com.company;
 public class BitCounting {
 
     public static int countBits(int n){
-
-        return 0;
+        String s = Integer.toBinaryString(n);
+        System.out.println(s);
+        int result = 0;
+        for(int i =0;i<s.length();i++){
+            if(s.charAt(i) == '1'){
+                result++;
+            }
+        }
+        return result;
     }
 }
