0b7de7325c7044e0f874c92451ee9fa6d3d66987
KevinW <bird831205@hotmail.com>
2020-02-18 08:47:28 -0500

    simple peaks implementation

--- src/main/java/com/company/PickPeaks.java
+++ src/main/java/com/company/PickPeaks.java
@@ -1,11 +1,24 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PickPeaks {
     public static Map<String, List<Integer>> getPeaks(int[] arr) {
         // Your code here!
-        return null;
+        Map<String, List<Integer>> result = new HashMap<>();
+        result.put("pos",new ArrayList<>());
+        result.put("peaks", new ArrayList<>());
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                List<Integer> pos = result.get("pos");
+                List<Integer> peak = result.get("peaks");
+                pos.add(i);
+                peak.add(arr[i]);
+            }
+        }
+        return result;
     }
 }
