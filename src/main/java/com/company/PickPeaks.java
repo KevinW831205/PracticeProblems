fbc10c1b352913f66eb5af398c3a6a73c0f8b8d0
KevinW <bird831205@hotmail.com>
2020-02-18 08:58:49 -0500

    account for pleateaus

--- src/main/java/com/company/PickPeaks.java
+++ src/main/java/com/company/PickPeaks.java
@@ -9,9 +9,28 @@ public class PickPeaks {
     public static Map<String, List<Integer>> getPeaks(int[] arr) {
         // Your code here!
         Map<String, List<Integer>> result = new HashMap<>();
-        result.put("pos",new ArrayList<>());
+        result.put("pos", new ArrayList<>());
         result.put("peaks", new ArrayList<>());
+        int pleatauPos = 0;
+        int pleatauPeak = 0;
+        boolean pleatau = false;
+
         for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                if (!pleatau) {
+                    pleatauPos = i;
+                    pleatauPeak = arr[i];
+                }
+                pleatau = true;
+            } else {
+                if(pleatau && arr[i] > arr[i+1] ){
+                    List<Integer> pos = result.get("pos");
+                    List<Integer> peak = result.get("peaks");
+                    pos.add(pleatauPos);
+                    peak.add(pleatauPeak);
+                }
+                pleatau = false;
+            }
             if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
                 List<Integer> pos = result.get("pos");
                 List<Integer> peak = result.get("peaks");
