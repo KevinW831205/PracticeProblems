ca96d6eda6b46cbc0d4f769ad02f53112be5780a
KevinW <bird831205@hotmail.com>
2020-02-18 08:53:53 -0500

    pleatau bug

--- src/test/java/com/company/PickPeaksTest.java
+++ src/test/java/com/company/PickPeaksTest.java
@@ -17,27 +17,36 @@ public class PickPeaksTest {
     }
 
     @Test
-    public void test1(){
+    public void test1() {
         Map<String, List<Integer>> expected = new HashMap<>();
-        expected.put("pos", Arrays.stream(new int[]{3,7}).boxed().collect(Collectors.toList()) );
-        expected.put("peaks", Arrays.stream(new int[]{6,3}).boxed().collect(Collectors.toList()));
-        peakTest(expected ,new int[]{1,2,3,6,4,1,2,3,2,1});
+        expected.put("pos", Arrays.stream(new int[]{3, 7}).boxed().collect(Collectors.toList()));
+        expected.put("peaks", Arrays.stream(new int[]{6, 3}).boxed().collect(Collectors.toList()));
+        peakTest(expected, new int[]{1, 2, 3, 6, 4, 1, 2, 3, 2, 1});
     }
 
     @Test
-    public void test2(){
+    public void test2() {
         Map<String, List<Integer>> expected = new HashMap<>();
-        expected.put("pos", Arrays.stream(new int[]{2}).boxed().collect(Collectors.toList()) );
+        expected.put("pos", Arrays.stream(new int[]{2}).boxed().collect(Collectors.toList()));
         expected.put("peaks", Arrays.stream(new int[]{3}).boxed().collect(Collectors.toList()));
-        peakTest(expected ,new int[]{2,1,3,1,2,2,2,2});
+        peakTest(expected, new int[]{2, 1, 3, 1, 2, 2, 2, 2});
     }
 
     @Test
-    public void test3(){
+    public void test3() {
         Map<String, List<Integer>> expected = new HashMap<>();
-        expected.put("pos", Arrays.stream(new int[]{3,5,7,11}).boxed().collect(Collectors.toList()) );
-        expected.put("peaks", Arrays.stream(new int[]{4,4,4,2}).boxed().collect(Collectors.toList()));
-        peakTest(expected ,new int[]{1,2,3,4,3,4,3,4,3,2,1,2,1});
+        expected.put("pos", Arrays.stream(new int[]{3, 5, 7, 11}).boxed().collect(Collectors.toList()));
+        expected.put("peaks", Arrays.stream(new int[]{4, 4, 4, 2}).boxed().collect(Collectors.toList()));
+        peakTest(expected, new int[]{1, 2, 3, 4, 3, 4, 3, 4, 3, 2, 1, 2, 1});
     }
+
+    @Test
+    public void test4() {
+        Map<String, List<Integer>> expected = new HashMap<>();
+        expected.put("pos", Arrays.stream(new int[]{3, 7, 10}).boxed().collect(Collectors.toList()));
+        expected.put("peaks", Arrays.stream(new int[]{6, 3, 2}).boxed().collect(Collectors.toList()));
+        peakTest(expected, new int[]{3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1});
+    }
+
 
 }
