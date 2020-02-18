e0c35aff3bc14aee399ce75b6c43f170660f1dd0
KevinW <bird831205@hotmail.com>
2020-02-18 08:40:57 -0500

    peak test

---
+++ src/main/java/com/company/PickPeaks.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.List;
+import java.util.Map;
+
+public class PickPeaks {
+    public static Map<String, List<Integer>> getPeaks(int[] arr) {
+        // Your code here!
+        return null;
+    }
+}
