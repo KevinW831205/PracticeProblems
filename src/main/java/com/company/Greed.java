f3bd0a9cf8d2c921b04524f92389dac2e4bbf393
KevinW <bird831205@hotmail.com>
2020-02-20 08:33:03 -0500

    simple implementation

--- src/main/java/com/company/Greed.java
+++ src/main/java/com/company/Greed.java
@@ -1,8 +1,64 @@
 package com.company;
 
 public class Greed {
-    public static int greedy(int[] dice){
+    public static int greedy(int[] dice) {
         //code here
-        return 0;
+        int score = 0;
+
+        int oneCount = count(1, dice);
+        while (oneCount >= 3) {
+            score += 1000;
+            oneCount -= 3;
+        }
+        while (oneCount > 0) {
+            score += 100;
+            oneCount--;
+        }
+
+        int twoCount = count(2, dice);
+        while (twoCount >= 3) {
+            score += 200;
+            twoCount--;
+        }
+
+        int threeCount = count(3, dice);
+        while (threeCount >= 3) {
+            score += 300;
+            threeCount--;
+        }
+
+        int fourCount = count(4, dice);
+        while (fourCount >= 3) {
+            score += 400;
+            fourCount--;
+        }
+
+        int fiveCount = count(5, dice);
+        while (fiveCount >= 3) {
+            score += 500;
+            fiveCount--;
+        }
+        while (fiveCount > 0) {
+            score += 50;
+            fiveCount--;
+        }
+
+        int sixCount = count(6,dice);
+        while (sixCount>6){
+            score+=600;
+            sixCount--;
+        }
+
+        return score;
+    }
+
+    private static int count(int number, int[] dice) {
+        int count = 0;
+        for (int die : dice) {
+            if (die == number) {
+                count++;
+            }
+        }
+        return count;
     }
 }
