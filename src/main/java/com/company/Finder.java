57d853a1781c091d88f7afd4fdbbc1da4cfa3224
KevinW <bird831205@hotmail.com>
2020-02-21 10:24:08 -0500

    wrapup

--- src/main/java/com/company/Finder.java
+++ src/main/java/com/company/Finder.java
@@ -1,9 +1,35 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.stream.Collector;
+
 public class Finder {
 
     static boolean pathFinder(String maze) {
         // Your code here!!
+
+        String[] s =  {};
+
+        String[] mazeRow = maze.split("\n");
+        String[][] mazeArr = Arrays.stream(mazeRow)
+                .map(r -> r.split(""))
+                .toArray(String[][]{} );
+
+        for(String[] row : mazeArr){
+            for(String c : row){
+                System.out.println(c);
+            }
+        }
+
         return false;
+    }
+
+    class maze {
+
+
+    }
+
+    class node {
+
     }
 }
