02ffcf7bd2bbfaabdb1ef37abe80aeda94bad127
KevinW <bird831205@hotmail.com>
2020-02-24 09:16:10 -0500

    moving opennode to closed nodes

--- src/main/java/com/company/Finder.java
+++ src/main/java/com/company/Finder.java
@@ -1,40 +1,68 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class Finder {
 
+    static String[][] mazeArr;
+    static Set<Node> openNodes = new HashSet<>();
+    static Set<Node> closedNodes = new HashSet<>();
+
+
     static boolean pathFinder(String maze) {
         // Your code here!!
 
-        Set<Node> moveAbleNode;
-
-        String[] s = {};
 
         String[] mazeRow = maze.split("\n");
-        String[][] mazeArr = Arrays.stream(mazeRow)
+        mazeArr = Arrays.stream(mazeRow)
                 .map(r -> r.split(""))
                 .toArray(String[][]::new);
 
+        Node startingNode = new Node(0,0);
+        openNodes.add(startingNode);
+        while(!openNodes.isEmpty()){
+            for(Node node : openNodes){
+                System.out.println(node);
+                openNodes.remove(node);
+                Integer x = node.getPosx();
+                Integer y = node.getPosy();
 
+                addOpenNodes(x-1,y);
+                addOpenNodes(x+1,y);
+                addOpenNodes(x,y-1);
+                addOpenNodes(x,y+1);
+                closedNodes.add(node);
+            }
+        }
 
         return false;
     }
 
+    private static void addOpenNodes(int x, int y){
+        try{
+            if(mazeArr[x][y].equals(".")){
+                Node nodeToAdd = new Node(x,y);
+                if(!closedNodes.contains(nodeToAdd)){
+                    openNodes.add(nodeToAdd);
+                }
+            }
+        }catch (ArrayIndexOutOfBoundsException e){
 
-
+        }
+    }
 }
 
-class Node{
-    Integer posx;
-    Integer posy;
-    Set<Node> movableNodes;
 
-    public Node(Integer posx, Integer posy, Set<Node> movableNodes) {
+class Node{
+    private Integer posx;
+    private Integer posy;
+
+    public Node(Integer posx, Integer posy) {
         this.posx = posx;
         this.posy = posy;
-        this.movableNodes = movableNodes;
     }
 
     public Integer getPosx() {
@@ -45,7 +73,17 @@ class Node{
         return posy;
     }
 
-    public Set<Node> getMovableNodes() {
-        return movableNodes;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return Objects.equals(posx, node.posx) &&
+                Objects.equals(posy, node.posy);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(posx, posy);
     }
 }
