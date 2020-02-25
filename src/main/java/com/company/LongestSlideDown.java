afbec3ed10853175c8cf612ad878923ebee5d151
KevinW <bird831205@hotmail.com>
2020-02-25 09:01:09 -0500

    some pyramid orientation

--- src/main/java/com/company/LongestSlideDown.java
+++ src/main/java/com/company/LongestSlideDown.java
@@ -5,7 +5,55 @@ public class LongestSlideDown {
     public static int longestSlideDown(int[][] pyramid) {
         // Code Goes Here..
 
+        Pyramid cPyramid = new Pyramid(new PyramidNode(pyramid[0][0]));
 
+
+
+
+        return 0;
+    }
+
+
+}
+
+class PyramidNode {
+    private PyramidNode leftChild;
+    private Pyramid rightChild;
+    private int value;
+
+    public PyramidNode(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public PyramidNode getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(PyramidNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public Pyramid getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(Pyramid rightChild) {
+        this.rightChild = rightChild;
+    }
+}
+
+class Pyramid {
+    PyramidNode root;
+
+    public Pyramid(PyramidNode root) {
+        this.root = root;
+    }
+
+    public int findMaxSum() {
         return 0;
     }
 
