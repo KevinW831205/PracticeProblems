388de293c42271fbc1e1555a54f61948c86835d9
KevinW <bird831205@hotmail.com>
2020-02-26 11:19:46 -0500

    test cases

--- src/test/java/com/company/CarMileageTest.java
+++ src/test/java/com/company/CarMileageTest.java
@@ -1,5 +1,38 @@
 package com.company;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CarMileageTest {
+
+    @Test
+    public void testTooSmall() {
+        Assert.assertEquals(0, CarMileage.isInteresting(3, new int[]{1337, 256}));
+    }
+
+    @Test
+    public void testAlmostAwesome() {
+        Assert.assertEquals(1, CarMileage.isInteresting(1336, new int[]{1337, 256}));
+    }
+
+    @Test
+    public void testAwesome() {
+        Assert.assertEquals(2, CarMileage.isInteresting(1337, new int[]{1337, 256}));
+    }
+
+    @Test
+    public void testFarNotInteresting() {
+        Assert.assertEquals(0, CarMileage.isInteresting(11208, new int[]{1337, 256}));
+    }
+
+    @Test
+    public void testAlmostInteresting() {
+        Assert.assertEquals(1, CarMileage.isInteresting(11209, new int[]{1337, 256}));
+    }
+
+    @Test
+    public void testInteresting() {
+        Assert.assertEquals(2, CarMileage.isInteresting(11211, new int[]{1337, 256}));
+    }
 
 }
