dadd40e409d00c8a553ad180d56d5ff6349d9597
KevinW <bird831205@hotmail.com>
2020-02-26 09:00:42 -0500

    simple implementation and testing

--- src/main/java/com/company/FactDecomp.java
+++ src/main/java/com/company/FactDecomp.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -10,21 +9,40 @@ public class FactDecomp {
         // your code
         SortedMap<Integer, Integer> result = new TreeMap<>();
         for (int i = 2; i <= n; i++) {
-
+            SortedMap<Integer, Integer> pf = primeFactors(i);
+            pf.forEach((key, value) -> {
+                result.put(key, result.getOrDefault(key, 0) + value);
+            });
         }
 
+        StringBuilder sb = new StringBuilder();
+        result.forEach((key,value)->{
+            sb.append(key);
+            if(value>2){
+                sb.append("^"+value);
+            }
+            sb.append(" * ");
+        });
+
+        return sb.substring(0,sb.length()-3);
     }
 
-    public Map<Integer, Integer> primeFactors(int n) {
+    private static SortedMap<Integer, Integer> primeFactors(int n) {
         SortedMap<Integer, Integer> result = new TreeMap<>();
 
         while (n % 2 == 0) {
             result.put(2, result.getOrDefault(2, 0) + 1);
+            n /= 2;
+        }
+        for (int i = 3; i <= n; i += 2) {
+            if (isPrime(i) && n % i == 0) {
+                result.put(i, 1);
+            }
         }
         return result;
     }
 
-    private boolean isPrime(int n) {
+    private static boolean isPrime(int n) {
         for (int i = 2; i < n / 2; i++) {
             if (n % i == 0) {
                 return false;
--- src/test/java/com/company/FactDecompTest.java
+++ src/test/java/com/company/FactDecompTest.java
@@ -1,4 +1,45 @@
 package com.company;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FactDecompTest {
+
+//    @Test
+//    public void pfTest() {
+//        SortedMap<Integer, Integer> actual = FactDecomp.primeFactors(6);
+//        SortedMap<Integer, Integer> expected = new TreeMap<>();
+//        expected.put(2, 1);
+//        expected.put(3, 1);
+//        Assert.assertEquals(expected, actual);
+//    }
+
+    public void factDecomp(String expected, int input){
+        String actual = FactDecomp.decomp(input);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void test1(){
+        factDecomp("2^15 * 3^6 * 5^3 * 7^2 * 11 * 13 * 17",17);
+    }
+
+    @Test
+    public void test2(){
+        factDecomp("2^19 * 3^9 * 5^4 * 7^3 * 11^2 * 13 * 17 * 19",22);
+    }
+    @Test
+    public void test3(){
+        factDecomp("2^15 * 3^6 * 5^3 * 7^2 * 11 * 13 * 17",5);
+    }
+    @Test
+    public void test4(){
+        factDecomp("2^11 * 3^5 * 5^2 * 7^2 * 11 * 13",14);
+    }
+    @Test
+    public void test5(){
+        factDecomp("2^22 * 3^10 * 5^6 * 7^3 * 11^2 * 13 * 17 * 19 * 23",25);
+    }
+
+
 }
