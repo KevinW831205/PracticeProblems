636e1f06b87d61d1112bcd97249fe07e3dcc6b6e
Kevin Wang <bird831205@hotmail.com>
2020-03-02 08:58:14 -0500

    simple implementation

--- src/main/java/com/company/CamelCase.java
+++ src/main/java/com/company/CamelCase.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class CamelCase {
 
     static int camelcase(String s) {
-
+        return (int) (Arrays.stream(s.split(""))
+                        .filter(c->c.matches("[A-Z]"))
+                        .count() + 1);
 
     }
 }
