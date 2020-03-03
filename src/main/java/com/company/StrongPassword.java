0e5ca954b20cb977296feb9147b5f68bb69778f5
Kevin Wang <bird831205@hotmail.com>
2020-03-03 08:14:13 -0500

    stringpasstest

---
+++ src/main/java/com/company/StrongPassword.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class StrongPassword {
+
+    static int minimumNumber(int n, String password) {
+        // Return the minimum number of characters to make the password strong
+
+        return -1;
+    }
+}
---
+++ src/test/java/com/company/StrongPasswordTest.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StrongPasswordTest {
+
+    public void passTest(int expected,String input){
+        int actual = StrongPassword.minimumNumber(input.length(), input);
+        Assert.assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void test1(){
+        passTest(3, "Ab1");
+    }
+
+    @Test
+    public void test2(){
+        passTest(1, "#Asdfqer");
+    }
+
+
+
+
+}
