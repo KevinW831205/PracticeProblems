011215132e0e153465291baa071e31d33739297b
Kevin Wang <bird831205@hotmail.com>
2020-03-03 08:23:22 -0500

    simple implementation

--- src/main/java/com/company/StrongPassword.java
+++ src/main/java/com/company/StrongPassword.java
@@ -5,6 +5,21 @@ public class StrongPassword {
     static int minimumNumber(int n, String password) {
         // Return the minimum number of characters to make the password strong
 
-        return -1;
+        int minToMeetLength = Math.max(6-password.length(),0);
+
+        int bonusLength = 0;
+        if(!password.matches(".*[!@#\\$%\\^&\\*\\(\\)-\\+].*")){
+            bonusLength++;
+        }
+        if(!password.matches(".*[A-Z].*")){
+            bonusLength++;
+        }
+        if(!password.matches(".*[a-z].*")){
+            bonusLength++;
+        }
+        if(!password.matches(".*[0-9].*")){
+            bonusLength++;
+        }
+        return Math.max(bonusLength,minToMeetLength);
     }
 }
