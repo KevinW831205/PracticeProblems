ee1b0ceabb08a4ef6039e0977c29f3663fc52f70
KevinW <bird831205@hotmail.com>
2020-03-31 15:37:37 -0400

    simple implementation

--- src/main/java/com/company/TreeByLevels.java
+++ src/main/java/com/company/TreeByLevels.java
@@ -1,13 +1,36 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TreeByLevels {
 
-    public static List<Integer> treeByLevels(Node node)
-    {
+    public static List<Integer> treeByLevels(Node node) {
+        List<Integer> result = new ArrayList<>();
+        if (node == null) {
+            return result;
+        }
 
+        List<Node> nodeNextLevel = new ArrayList<>();
+        nodeNextLevel.add(node);
 
-        return null;
+        while (!nodeNextLevel.isEmpty()) {
+
+            List<Node> temp = new ArrayList<>(nodeNextLevel);
+
+            nodeNextLevel.clear();
+            for(Node n : temp){
+                result.add(n.value);
+                if (n.left != null) {
+                    nodeNextLevel.add(n.left);
+                }
+                if (n.right != null) {
+                    nodeNextLevel.add(n.right);
+                }
+            }
+        }
+        return result;
     }
+
+
 }
