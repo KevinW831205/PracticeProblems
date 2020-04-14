3c7aefc01cdbc87e688cba546865105fda34e019
KevinW <bird831205@hotmail.com>
2020-04-14 08:39:52 -0400

    update

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,53 +1,14 @@
 package com.company;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-//        List<String> l1 = new ArrayList<String>();
-//
-//      639 000 000 000
-//
-//        l1.add("a");
-//        l1.add("a");
-//        l1.add("b");
-//        l1.remove("a");
-//
-//        System.out.println(l1);
-
-//        int x = Integer.MAX_VALUE;
-//        System.out.println("x " +x);
-//        System.out.println("x+1 "+(x+1));
 
     }
 
-    public static Integer degreeOfArray(String input){
 
-        String[] sArr = input.split(" ");
-        Integer[] arr = new Integer[sArr.length];
-
-        for(int i=0;i<sArr.length;i++){
-            arr[i]=Integer.parseInt(sArr[i]);
-        }
-        Arrays.sort(arr);
-
-        Integer maxCount = 0;
-        Integer count = 0;
-        Integer maxValue = null;
-        for(int i =0 ;i<arr.length-1; i++){
-            if(arr[i] == arr[i+1]){
-                count++;
-                if(count> maxCount){
-                    maxCount = count;
-                    maxValue = arr[i];
-                }
-            } else {
-                count = 0;
-            }
-        }
-        return maxValue;
-    }
 
 }
