3302cf39f4ad59d865e15ed1e01a83ab5a46cc82
Kevin Wang <bird831205@hotmail.com>
2022-09-06 23:32:12 -0400

    practice setup

---
+++ src/main/java/com/company/ZigZagConverter.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class ZigZagConverter {
+    public static String convert(String s, int numRows) {
+
+        /*
+            N = 3
+                r = 0 c 0 ,4, 8, 12
+                r = 1 c 1, 3 ,5 ,7 ,9
+                r = 2 c 2, 6 ,10
+
+            N = 4
+                r = 0 c 0, 6, 12
+                r = 1
+         */
+
+        String[] rows = new String[numRows];
+
+
+        return null;
+    }
+}
---
+++ src/test/java/com/company/ZigZagConverterTest.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ZigZagConverterTest {
+
+    public void ZigZagConverterTest(String expected, String s, int rows){
+
+        String actual = ZigZagConverter.convert(s,rows);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void test1() {
+        ZigZagConverterTest("PAHNAPLSIIGYIR", "PAYPALISHIRING", 3);
+    }
+    @Test
+    public void test2() {
+        ZigZagConverterTest("PAYPALISHIRING", "PINALSIGYAHRPI", 4);
+    }
+    @Test
+    public void test3() {
+        ZigZagConverterTest("A", "A", 1);
+    }
+}
