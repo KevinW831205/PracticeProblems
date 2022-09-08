048404e00179089f394b7bd2460d42928fac7f5d
Kevin Wang <bird831205@hotmail.com>
2022-09-07 21:47:51 -0400

    practice

--- src/main/java/com/company/WaterContainer.java
+++ src/main/java/com/company/WaterContainer.java
@@ -2,6 +2,22 @@ package com.company;
 
 public class WaterContainer {
     public static int maxArea(int[] height) {
-        return -1;
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            if (height[left] > height[right]) {
+                int area = height[right] * (right - left);
+                max = Math.max(area, max);
+                right--;
+            } else {
+                int area = height[left] * (right - left);
+                max = Math.max(area, max);
+                left++;
+            }
+        }
+
+        return max;
     }
 }
